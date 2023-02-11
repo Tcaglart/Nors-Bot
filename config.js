20e9e0284bc0b584def749966f371a70b0be23de
Tcaglart <125141461+Tcaglart@users.noreply.github.com>
2023-02-11 18:41:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "", 
+    token: "MTA3Mzk5MTUyNzEwNzQwMzkzNw.GJKHQJ.Lyqmzprs58F0ZT_m52X4IWUrv5E8zcfjQ9N-Zc",
     prefix: ".",
-    destek: "",
-    topgg: "",
-    davet: "",
-    website: ""
+    destek: "https://discord.gg/nors",
+    topgg: "https://top.gg/bot/681137419663441933",
+    davet: "https://discord.com/oauth2/authorize?client_id=681137419663441933&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2FsZEQYsK&response_type=code&scope=bot%20guilds",
+    website: "https://norsbot.xyz/"
 }
 
